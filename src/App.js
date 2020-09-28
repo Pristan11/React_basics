3881277532d1f34e0a45f64df77e4bf1567a9699
Pristan <pristanse@gmail.com>
2020-09-28 12:04:01 +0530

    this is master branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
- 
+  
 
 
 function App() {
   return (
     <div className="App">
           <div>Now the react package is ready to work the unnessesory folders are deteled successfully </div>
-     </div>
+      </div>
   );
 }
 
